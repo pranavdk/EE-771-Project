48a47136ff915b82f5b29370d0aa50d317939ba3
Pranav Kulkarni <pranavkulkarni@Pranavs-MacBook-Pro.local>
2019-03-09 16:51:08 +0530

    correct KSVD

--- code/myKSVD.m
+++ code/myKSVD.m
@@ -1,43 +1,46 @@
 %% Implementation of K-SVD algorithm
 
-function [D,X] = myKSVD(D_init,Y,phi,max_iter,omp_iter)
+function [D,X] = myKSVD(D_init,Y,max_iter,omp_iter)
+% OMP iter is T0
 
 K = size(D_init,2);
-p = size(D_init,1);
-m = size(Y,1);
 N = size(Y,2);
 S = zeros(K,N);
-D_prev = D_init;
+
 for iter = 1:max_iter
-%step 1
+    iter
+%step 1 : Sparse coding
     for j = 1:N
         size(D_init);
         size(Y(:,j));
-        r = myOMP(Y(:,j),phi(:,:,j)*D_init,omp_iter);
+        r = myOMP(Y(:,j),D_init,omp_iter);
         S(:,j) = r;
     end
 
-%step 2
+%step 2 : Codebobk update
 
     for k = 1:K
-       sup = find(S(k,:));
-       M = zeros(size(Y));
-       G = zeros(p,p);
-       b = zeros(p,1);
-       for i = sup
-          M(:,i) = Y(:,i) - phi(:,:,i) * (D_init*S(:,i) -  D_init(:,k)*S(k,i));
-          G = G + (S(k,i)^2) * phi(:,:,i)' * phi(:,:,i); 
-          b = b +  S(k,i) * phi(:,:,i)' * M(:,i);
+        sup = find(S(k,:)); 
+       
+       Ek = Y;
+       for i = 1:K
+           if (i ~= k)
+               Ek = Ek -  D_init(:,i)*S(i,:);
+           end
        end
-       D_init(:,k) = G\b;
-       for i = sup
-          S(k,i) = (M(:,i)' * phi(:,:,i) * D_init(:,k))/(sum((phi(:,:,i)*D_init(:,k)).^2));
-       end
+       Ekr = Ek (:,sup);
+       
+       [Us,Ss,Vs] = svds(Ekr,1);
+       
+       D_init(:,k) = Us;
+       
+       S(k,sup) = Ss*Vs';
 
     end
-%     D_init = normc(D_init);
-    sum(sum((D_init - D_prev).^2));
-    D_prev = D_init;
+    
+    D_init = normc(D_init);
+    sum(sum((Y - D_init*S).^2) / sum(Y.^2))
+
 end
 
 D = D_init;
