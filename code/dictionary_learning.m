5be6d2f6c0e1b506fe1b56b1d3757c0b7c0b8227
Pranav Kulkarni <pranav.dk212@gmail.com>
2019-03-09 22:30:40 +0530

    dictionary learning complete

---
+++ code/dictionary_learning.m
@@ -0,0 +1,81 @@
+%% Dictionanry Learning
+
+clear;
+clc;
+close all;
+
+%% Global variables
+
+file_path = '../data/final.mp4';
+v = VideoReader(file_path);
+
+N_videos = 20;
+temporal_depth = 36;
+img_height = v.H;
+img_width = v.W;
+total_frames = floor(v.D*v.FR);
+subsmapling_rate = 2;
+
+%% Reading Video Segments and Data Preprocessing
+
+% C = randi([0,total_frames-temporal_depth],1,N_videos);
+% 
+% Data = zeros(N_videos,img_height/subsmapling_rate,img_width/subsmapling_rate,3,temporal_depth);
+% 
+% for i = 1:N_videos
+%     i
+%     video_segment = read(v,[C(i),C(i)+temporal_depth-1]);
+%     Data(i,:,:,:,:) = video_segment(1:subsmapling_rate:end,1:subsmapling_rate:end,:,:);    
+% end
+% 
+% save('../data/Videos20.mat','Data','-v7.3');
+
+%%
+
+mobj = matfile('../data/Videos20.mat');
+Video_Data = mobj.Data;
+m = size(Video_Data, 2);
+n = size(Video_Data, 3);
+patchsize = 8;
+stride = patchsize;
+num_patches = ((m-patchsize)/stride+1)*((n-patchsize)/stride+1);
+
+Video_Data = mean(Video_Data,4); % remove for colored images
+
+Dictionary = [];
+for vid_seg_index= 1:size(Video_Data,1)
+    vid_seg_index
+    data_array = zeros(8*num_patches,(patchsize^2)*temporal_depth);
+    count = 1;
+
+    for i = 1:stride:m
+        for j = 1:stride:n
+
+            temp = Video_Data(vid_seg_index,i:i+patchsize-1,j:j+patchsize-1,:,:);
+            temp_size = size(temp);
+
+            temp = reshape(temp,temp_size(2:end));
+            temp_flip = flip(temp,4);
+            data_array(count,:) = temp(:);
+            count = count +1;
+            data_array(count,:) = temp_flip(:);
+            count = count +1;
+
+            for rot_index = 1:3
+                temp = rot90(temp);
+                temp_flip = flip(temp,4);
+                data_array(count,:) = temp(:);
+                count = count +1;
+                data_array(count,:) = temp_flip(:);
+                count = count +1;
+            end      
+        end
+    end
+
+    params.data = data_array';
+    params.Tdata = 40;
+    params.dictsize = 625;
+    [final_basis,sparse_repr] = ksvd(params,'i');
+    
+    Dictionary = [Dictionary, final_basis];
+end
