75af655689d8fc8fef6a1108565ad02e7b3bc062
akagrecha <akagrecha@gmail.com>
2019-03-17 12:07:21 +0530

    Generating Coded Images

--- code/gen_sampling_matrix.m
+++ code/gen_sampling_matrix.m
@@ -3,12 +3,12 @@ function [sampling_matrix] = gen_sampling_matrix(temporal_depth, subsampling_rat
     %   Detailed explanation goes here
     img_size = floor(img_size/subsampling_rate);
     timemap = randi([1 temporal_depth], [img_size(1) img_size(2)]);
-    sampling_matrix = zeros(img_size(1), img_size(2), temporal_depth);
+    sampling_matrix = zeros(img_size(1), img_size(2), 3, temporal_depth);
     for i=1:img_size(1)
         for j = 1:img_size(2)
             for k = 1:bump_length
                 ind = mod(timemap(i,j)-1+k-1, temporal_depth) + 1;
-                sampling_matrix(i,j,ind) = 1;
+                sampling_matrix(i,j,:,ind) = 1;
             end
         end
     end
